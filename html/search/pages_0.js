c2970e29dc813369e50733272331db6bc30b036e
Travis CI <travis@travis-ci.org>
2018-10-02 13:24:04 +0000

    Deploy code docs to GitHub Pages Travis build: 3
    Commit: ebd7139d69927f1927ac5defa137066cfb391944

--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_20htu21d_2df_20humidity_20_2b_20temperature_20sensor_20for_20arduino_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fhtu21df_5flibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fhtu21df_5flibrary_29',['Adafruit HTU21D-F Humidity + Temperature Sensor for Arduino [![Build Status](https://travis-ci.org/adafruit/Adafruit_HTU21DF_Library.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_HTU21DF_Library)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20htu21d_2df_20humidity_2ftemp_20sensor_20for_20arduino_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fhtu21df_5flibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fhtu21df_5flibrary_29',['Adafruit HTU21D-F Humidity/Temp Sensor for Arduino [![Build Status](https://travis-ci.org/adafruit/Adafruit_HTU21DF_Library.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_HTU21DF_Library)',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
