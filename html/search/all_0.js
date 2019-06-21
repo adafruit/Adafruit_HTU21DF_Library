86aba73b9c49a1d009b8eeeb202a25725d9d78e9
Travis CI <travis@travis-ci.org>
2019-06-21 08:29:51 +0000

    Deploy code docs to GitHub Pages Travis build: 9
    Commit: 4c90679095303e19938997ba5c624f846c9e2205

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['adafruit_5fhtu21df',['Adafruit_HTU21DF',['../class_adafruit___h_t_u21_d_f.html',1,'Adafruit_HTU21DF'],['../class_adafruit___h_t_u21_d_f.html#aaed5d32a58f5f7f14feceb9aad010d16',1,'Adafruit_HTU21DF::Adafruit_HTU21DF()']]],
-  ['adafruit_20htu21d_2df_20humidity_2ftemp_20sensor_20for_20arduino_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fhtu21df_5flibrary_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fhtu21df_5flibrary_29',['Adafruit HTU21D-F Humidity/Temp Sensor for Arduino [![Build Status](https://travis-ci.org/adafruit/Adafruit_HTU21DF_Library.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_HTU21DF_Library)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_5fhtu21df',['Adafruit_HTU21DF',['../class_adafruit___h_t_u21_d_f.html',1,'Adafruit_HTU21DF'],['../class_adafruit___h_t_u21_d_f.html#aaed5d32a58f5f7f14feceb9aad010d16',1,'Adafruit_HTU21DF::Adafruit_HTU21DF()']]]
 ];
