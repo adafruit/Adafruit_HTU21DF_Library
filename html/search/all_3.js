4507ba5c43c12aca89980dde3a680ef739dee8d4
Doxygen CI <travis@travis-ci.org>
2020-05-27 16:18:58 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

---
+++ html/search/all_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['readhumidity',['readHumidity',['../class_adafruit___h_t_u21_d_f.html#ad5e40dd39b2bbf757ea1353b68330eb4',1,'Adafruit_HTU21DF']]],
+  ['readtemperature',['readTemperature',['../class_adafruit___h_t_u21_d_f.html#afeb534013eef7ae07d167e4158d2b458',1,'Adafruit_HTU21DF']]],
+  ['reset',['reset',['../class_adafruit___h_t_u21_d_f.html#a0f41425f85e149db03a1770acea11281',1,'Adafruit_HTU21DF']]]
+];
---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['adafruit_20htu21df_20sensor',['Adafruit HTU21DF Sensor',['../index.html',1,'']]]
+];
