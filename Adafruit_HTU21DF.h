fb9323f5d0bb15c852928d6d0664fbe33b9cec06
dherrada <=>
2020-05-27 12:15:33 -0400

    Fixed doxygen index page

--- Adafruit_HTU21DF.h
+++ Adafruit_HTU21DF.h
@@ -1,18 +1,6 @@
-/***************************************************
-  This is a library for the HTU21D-F Humidity & Temp Sensor
-
-  Designed specifically to work with the HTU21D-F sensor from Adafruit
-  ----> https://www.adafruit.com/products/1899
-
-  These displays use I2C to communicate, 2 pins are required to
-  interface
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
+/*!
+ * @file Adafruit_HTU21DF.h
+ */
 
 #ifndef _ADAFRUIT_HTU21DF_H
 #define _ADAFRUIT_HTU21DF_H
